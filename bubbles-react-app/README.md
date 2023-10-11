325cd48525a8715131f4741c10d90a242cf3c080
Giovanni Tommasi <giovannitommasi95@gmail.com>
2023-10-12 00:24:48 +0200

    Added bubbles react app

---
+++ bubbles-react-app/README.md
@@ -0,0 +1,3 @@
+# Bubbles React
+
+This template provides a minimal setup to get React working with Bubbles UI web component.
---
+++ bubbles-react-app/src/Bubbles.jsx
@@ -0,0 +1,10 @@
+import '../../Bubbles'
+import './Bubbles.css'
+
+function Bubbles() {
+  return (
+    <bubbles-ui></bubbles-ui>
+  )
+}
+
+export default Bubbles
