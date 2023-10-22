529273f0962fcda6d3857e2f29410956dbe1dd53
Giovanni Tommasi <giovannitommasi95@gmail.com>
2023-10-22 13:33:45 +0200

    Added bubbles react

--- bubbles-react/src/main.jsx
+++ bubbles-react/src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Bubbles from './Bubbles.jsx'
+import Bubbles from './Bubbles'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('app')).render(
