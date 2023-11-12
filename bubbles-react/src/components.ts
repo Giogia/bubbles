58bbcff330885f6b458aec6f1e9498e0ed46708b
Giovanni Tommasi <giovannitommasi95@gmail.com>
2023-11-12 18:46:40 +0100

    Added Stencil Web Components to React App

---
+++ bubbles-react/src/components.ts
@@ -0,0 +1,10 @@
+/* eslint-disable */
+/* tslint:disable */
+/* auto-generated react proxies */
+import { createReactComponent } from './react-component-lib';
+
+import type { JSX } from 'bubbles-web-components/dist/components';
+
+import { defineCustomElement as defineBubblesUi } from 'bubbles-web-components/dist/components/bubbles-ui.js';
+
+export const BubblesUi = /*@__PURE__*/createReactComponent<JSX.BubblesUi, HTMLBubblesUiElement>('bubbles-ui', undefined, undefined, defineBubblesUi);
