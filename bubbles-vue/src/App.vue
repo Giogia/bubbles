45610478172aa15e24a053fb627eb93319af727b
Giovanni Tommasi <giovannitommasi95@gmail.com>
2023-12-28 21:03:41 +0100

    Added Stencil Web Components to Vue App

--- bubbles-vue/src/App.vue
+++ bubbles-vue/src/App.vue
@@ -3,7 +3,22 @@ import {BubblesUi} from './components'
 </script>
 
 <template>
-  <BubblesUi 
-    data="{ nodes: [], links: [] }"
-  />
+  <BubblesUi :nodes="nodes" :links="links">
+    <div>Test</div>
+    <div>Test</div>
+    <div>Test</div>
+  </BubblesUi>
 </template>
+
+<script lang="ts">
+export default {
+  data: () => ({
+    nodes: [
+      { id: "Node 0", radius: 0.1, x: 0, y: 0 },
+      { id: "Node 1", radius: 0.1, x: 0.25, y: 0.1 },
+      { id: "Node 2", radius: 0.25, x: 0.5, y: 0.35 },
+    ],
+    links: []
+  })
+}
+</script>
--- bubbles-vue/src/components.ts
+++ bubbles-vue/src/components.ts
@@ -5,10 +5,20 @@ import { defineContainer } from './vue-component-lib/utils';
 
 import type { JSX } from 'bubbles/dist/components';
 
+import { defineCustomElement as defineBubbleCard } from 'bubbles/dist/components/bubble-card.js';
 import { defineCustomElement as defineBubblesUi } from 'bubbles/dist/components/bubbles-ui.js';
 
 
+export const BubbleCard = /*@__PURE__*/ defineContainer<JSX.BubbleCard>('bubble-card', defineBubbleCard, [
+  'icon',
+  'name',
+  'description'
+]);
+
+
 export const BubblesUi = /*@__PURE__*/ defineContainer<JSX.BubblesUi>('bubbles-ui', defineBubblesUi, [
-  'data'
+  'nodes',
+  'links',
+  'center'
 ]);
 
